77ab527cc930e64aa38307c3f65f3e1b3f9204eb
trminhnam <trminhnam20082002@gmail.com>
2023-05-21 09:59:40 +0700

    fix exit

--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@ python trainer.py --mode train \
         --num_epochs 2 \
         --cuda \
         --batch_size 8 \
-        --train_input '/home/sreehs/dlt/table-to-text-generation/Data/totto_data_small/processed_totto_train_data_1000.jsonl' \
-        --development_input '/home/sreehs/dlt/table-to-text-generation/Data/totto_data_small/processed_totto_dev_data_500.jsonl'
+        --train_input 'Data/totto_data_small/processed_totto_train_data_1000.jsonl' \
+        --development_input 'Data/totto_data_small/processed_totto_dev_data_500.jsonl'
